1bd6fb160f23233fa0ee76d65d0019c6e7d6716b
Eirik S. Morland <eirik@morland.no>
2016-07-15 20:28:51 +0200

    Fix for d3 v4.

--- test/test.js
+++ test/test.js
@@ -7,13 +7,13 @@ var document = require('jsdom').jsdom();
 describe('All the things', function() {
   it('Should expose a function and return an array', function() {
     msg2svg.should.be.instanceOf(Function);
-    msg2svg(document).should.be.instanceOf(Array);
+    msg2svg(document)._groups.should.be.instanceOf(Array);
   });
   it('Should always return the same thing on same random', function() {
     var random = 'this is random seed';
     // Yeah, if we init this with a second param, it's a win for coverage.
     var two = msg2svg(document, random, {document: document});
     var expected = fs.readFileSync('./test/expected.txt').toString().trim();
-    expected.should.equal(two[0][0].innerHTML);
+    expected.should.equal(two._groups[0][0].innerHTML);
   });
 });
